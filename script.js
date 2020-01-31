0d2184be8979be671f4f9d4a22e91fbf9c1b0a48
silvestrevivo <silvestrevivomillan@gmail.com>
2020-01-31 11:28:00 +0100

    form finished

--- script.js
+++ script.js
@@ -1,1 +1,77 @@
-console.log('ready')
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// Show input error message
+function showError(input, message){
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+// Show success outline
+function showSuccess(input){
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+// Check email is valid
+function checkEmail(input){
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if(re.test(input.value.trim())){
+    showSuccess(input);
+  } else {
+    showError(input, `${getFieldName(input)} is not valid`);
+  }
+}
+
+// Check required fields
+function checkRequired(inputArray){
+  inputArray.forEach(input => {
+    if(input.value.trim() === ''){
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  })
+}
+
+// Check input Length
+function checkLength(input, min, max){
+  if(input.value.length < min){
+    showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+  } else if (input.value.length > max){
+    showError(input, `${getFieldName(input)} must be less than ${max} characters`)
+  } else {
+    showSuccess(input);
+  }
+}
+
+// Check passwords match
+function checkPassworsdMatch(input1, input2){
+  if(input1.value !== input2.value){
+    showError(input2, 'Passwords do not match');
+  }
+}
+
+// Get FieldName
+function getFieldName(input){
+  if(input.id === 'password2'){
+    return 'Confirm password';
+  }
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  checkRequired([username, email, password, password2]);
+
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPassworsdMatch(password, password2);
+})
